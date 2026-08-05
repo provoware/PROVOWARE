73d7a725956eec5cd60da623bc9617fa092d66a3
pppoppi <provoware.one@gmail.com>
2026-08-05 07:48:46 +0200

    test: Transferdialog und A11y-Ladereihenfolge statisch prüfen

--- tests/smoke/test_index.py
+++ tests/smoke/test_index.py
@@ -11,15 +11,23 @@ def test_index_contains_accessible_core_regions():
         'id="report-manager-button"', 'id="report-dialog"', 'id="report-preview-content"',
         'id="project-manager-button"', 'id="project-dialog"', 'id="project-new-form"',
         'id="project-filter"', 'id="project-action-checkbox"', 'id="current-project-name"',
+        'id="project-transfer-button"', 'id="transfer-dialog"', 'id="transfer-file"',
+        'id="transfer-checksum-status"', 'id="transfer-schema-status"', 'id="transfer-project-id"',
+        'id="transfer-existing-status"', 'id="transfer-conflict-count"', 'id="transfer-mode"',
+        'id="transfer-replace-name"', 'id="transfer-replace-checkbox"', 'id="transfer-apply-button"',
+        'id="transfer-audit-result"', 'data-arrow-navigation="vertical"',
         'data-report-format="markdown"', 'data-report-format="html"',
         'data-report-format="text"', 'data-report-format="json"',
         'src="js/migration-engine.js"', 'src="js/project-repository.js"',
-        'src="js/project-manager.js"', 'src="js/report-manager.js"'
+        'src="js/project-transfer.js"', 'src="js/accessibility.js"',
+        'src="js/project-manager.js"', 'src="js/project-transfer-manager.js"',
+        'src="js/report-manager.js"'
     ):
         assert marker in html
     assert html.index('src="js/migration-engine.js"') < html.index('src="js/storage-engine.js"')
-    assert html.index('src="js/storage-engine.js"') < html.index('src="js/project-repository.js"') < html.index('src="js/state-manager.js"')
-    assert html.index('src="js/report-generator.js"') < html.index('src="js/project-manager.js"') < html.index('src="js/app.js"')
+    assert html.index('src="js/storage-engine.js"') < html.index('src="js/project-repository.js"') < html.index('src="js/project-transfer.js"') < html.index('src="js/state-manager.js"')
+    assert html.index('src="js/report-generator.js"') < html.index('src="js/accessibility.js"') < html.index('src="js/project-manager.js"')
+    assert html.index('src="js/project-manager.js"') < html.index('src="js/project-transfer-manager.js"') < html.index('src="js/app.js"')
 
 
 def test_runtime_has_no_remote_assets():
