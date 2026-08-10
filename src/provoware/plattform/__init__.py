7920ba8c3f59166db6e64011c1182a3cad999c7f
pppoppi <provoware.one@gmail.com>
2026-08-10 09:12:31 +0200

    I013: Dateisystemprobe öffentlich exportieren

--- src/provoware/plattform/__init__.py
+++ src/provoware/plattform/__init__.py
@@ -1,5 +1,12 @@
 """Read-only Plattformgrenzen von PROVOWARE."""
 
+from provoware.plattform.dateisystem_probe import (
+    DateisystemProbe,
+    PfadArt,
+    ProbeStatus,
+    SegmentBefund,
+    pruefe_dateisystempfad,
+)
 from provoware.plattform.linux import (
     LinuxSystemProfil,
     LinuxSystemQuellen,
@@ -11,13 +18,18 @@ from provoware.plattform.linux import (
 from provoware.plattform.pfade import PfadPruefung, PfadStatus, pruefe_projektpfad
 
 __all__ = [
+    "DateisystemProbe",
     "LinuxSystemProfil",
     "LinuxSystemQuellen",
+    "PfadArt",
     "PfadPruefung",
     "PfadStatus",
     "PlattformStatus",
+    "ProbeStatus",
+    "SegmentBefund",
     "SessionArt",
     "erkenne_linux_systemprofil",
     "parse_os_release",
+    "pruefe_dateisystempfad",
     "pruefe_projektpfad",
 ]
